596be61c270a63ddb8571c211c4393bba84b23c0
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-15 19:10:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  not_empty_array = ["Steve", "John", "Kyle"]
+  not_empty_array = ["Steve", "John", "Kyle", "Harold"]
 end
 
 def add_element_to_end_of_array(array, element)
