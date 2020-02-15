073efd980aab61f50d195c3b471819f7c100ac9b
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-15 19:00:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  empty_array = []
 end
 
 def create_an_array
-  
+  not_empty_array = ["Steve", "John", "Kyle"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  not_empty_array << "Sally"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  not_empty_array.upshift("Jessica")
 end
 
 def remove_element_from_end_of_array(array)
