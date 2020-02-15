5ecf9227c84db435fcab093dd6cd5ed45615a5d9
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-15 19:15:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,29 +7,36 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  not_empty_array << "Sally"
+  names = ["john"]
+  names << "sally"
 end
 
 def add_element_to_start_of_array(array, element)
-  not_empty_array.upshift("Jessica")
+  names = ["John"]
+  names.unshift("Sally")
 end
 
 def remove_element_from_end_of_array(array)
-  not_empty_array.pop
+  names = ["john", "sally", "Steve"]
+  names.pop
 end
 
 def remove_element_from_start_of_array(array)
-  not_empty_array.shift
+  names = ["john", "sally", "Steve"]
+  names.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  not_empty_array[1]
+  names = ["john", "sally", "Steve"]
+  names[1]
 end
 
 def retrieve_first_element_from_array(array)
-  not_empty_array[0]
+  names = ["john", "sally", "Steve"]
+  names[0]
 end
 
 def retrieve_last_element_from_array(array)
-  not_empty_array[-1]
+  names = ["john", "sally", "Steve"]
+  names[-1]
 end
